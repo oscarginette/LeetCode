21583451eb8053a5ff1e2619c63e9722bc919ae3
Oscar Ginette <49130842+oscarginette@users.noreply.github.com>
2022-02-02 00:43:52 +0100

    Time: 340 ms (32.93%), Space: 51.6 MB (11.54%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.ts
@@ -0,0 +1,13 @@
+function isPalindrome(x: number): boolean {
+    if(x < 0 || (x !== 0 && x % 10 == 0))
+        return false;
+
+    let reverse = 0;
+    
+    while (x > reverse) {
+        reverse = reverse * 10 + x % 10;
+        x = ~~(x/10);
+    }
+    
+    return x === reverse || x === ~~(reverse/10);
+};
