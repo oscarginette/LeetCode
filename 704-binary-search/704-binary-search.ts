92a15d43e8d8baa49479c5f797dda5d1b02e3850
Oscar Ginette <49130842+oscarginette@users.noreply.github.com>
2022-03-04 09:50:11 +0100

    Time: 90 ms (66.85%), Space: 45.8 MB (44.01%) - LeetHub

---
+++ 704-binary-search/704-binary-search.ts
@@ -0,0 +1,14 @@
+function search(nums: number[], target: number): number {
+  let left: number = 0;
+  let right: number = nums.length - 1;
+
+  while (left <= right) {
+    const mid: number = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) return mid;
+    if (target < nums[mid]) right = mid - 1;
+    else left = mid + 1;
+  }
+
+  return -1;
+}
