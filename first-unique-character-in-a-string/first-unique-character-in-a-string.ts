823d28f683e1197a971083e2ccc84f8dbee9d95b
Oscar Ginette <49130842+oscarginette@users.noreply.github.com>
2022-03-28 21:23:53 +0200

    Time: 169 ms, Memory: 45.8 MB - LeetHub

---
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.ts
@@ -0,0 +1,16 @@
+function firstUniqChar(s: string): number {
+  const map: Map<string, number> = new Map();
+  for (let i = 0; i < s.length; i++) {
+    if (map.has(s[i])) {
+      map.set(s[i], map.get(s[i]) + 1);
+    } else {
+      map.set(s[i], 1);
+    }
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (map.get(s[i]) === 1) {
+      return i;
+    }
+  }
+  return -1;
+}
